ee7a46fbc2d020dd7c6dd930b12eb4bbe12faffe
xudoyberdi123 <xudikk.1@gmail.com>
2021-09-20 11:33:41 +0300

    samandar uchun services tog'irlandi

--- src/tg/api/v1/announce/services.py
+++ src/tg/api/v1/announce/services.py
@@ -6,9 +6,11 @@ from django.conf import settings
 from django.db import connection
 from base.utils.sqlpaginator import SqlPaginator
 from base.utils.db import dictfetchall, dictfetchone
+
 PER_PAGE = settings.PAGINATE_BY
 
-def all_announse(request, form, to, region_id):
+
+def all_announse(request, form, to, region_id, user_id=None):
     try:
         page = int(request.GET.get('page', 1))
     except:
@@ -31,7 +33,8 @@ def all_announse(request, form, to, region_id):
 
     with closing(connection.cursor()) as cursor:
         cursor.execute(
-            "SELECT count(1) as cnt from tg_announce")
+            f"""SELECT count(1) as cnt from tg_announce  inner join geo_region gr ON gr.id = {region_id} 
+    where {form} <= (price->>'from')::int and {to} >= (price->>'to')::int""")
         row = dictfetchone(cursor)
 
     if row:
@@ -43,41 +46,40 @@ def all_announse(request, form, to, region_id):
     pagging = paginator.get_paginated_response()
 
     return OrderedDict([
-        ('items', result),
+        ('item', result),
         ('meta', pagging)
     ])
 
-def one_product(request, id):
 
+def get_all_user(request,  user_id=None):
     try:
-        page =int(request.GET.get('page', 1))
+        page = int(request.GET.get('page', 1))
     except:
         page = 1
-
     offset = (page - 1) * PER_PAGE
 
     extra_sql = f"""
-        select ann.id, fullname, phone_number, coalesce(price->>'from', '') || '-' ||coalesce(price->>'to', '') as price, description, geo."name"->>'uz' as region_name_uz, geo."name"->>'ru' as region_name_ru, cat."name"->>'uz' as category_name_uz, cat."name"->>'ru' as category_name_ru
+   select ann.id, fullname, phone_number, coalesce(price->>'from', '') || '-' ||coalesce(price->>'to', '') as price, 
+   description, geo."name"->>'uz' as region_name_uz, geo."name"->>'ru' as region_name_ru,
+    cat."name"->>'uz' as category_name_uz, cat."name"->>'ru' as category_name_ru
     from tg_announce ann
-    inner join geo_region geo on ann.region_id = geo.id
-    inner join tg_announcecategories acat on ann.id = acat.resume_id 
-    inner join tg_category cat on acat.category_id = cat.id 
+    left join geo_region geo on ann.region_id = geo.id
+    left join tg_announcecategories acat on ann.id = acat.resume_id 
+    left join tg_category cat on acat.category_id = cat.id 
     where ann.user_id = %s and ann.is_active = True
+    order by ann.id desc
     limit %s offset %s
-    """
-
+"""
     with closing(connection.cursor()) as cursor:
-        cursor.execute(extra_sql, [id, PER_PAGE, offset])
+        cursor.execute(extra_sql, [user_id, PER_PAGE, offset])
         data = dictfetchone(cursor)
         if data:
             result = _format(data)
         else:
             result = None
-
     with closing(connection.cursor()) as cursor:
         cursor.execute(
-            "select count(1) as cnt from tg_announce ta  where ta.user_id = %s and ta.is_active = True", [id]
-        )
+            "SELECT count(1) as cnt from tg_announce where user_id = %s and is_active = True", [user_id])
         row = dictfetchone(cursor)
 
     if row:
@@ -87,11 +89,36 @@ def one_product(request, id):
 
     paginator = SqlPaginator(request, page=page, per_page=PER_PAGE, count=count_records)
     pagging = paginator.get_paginated_response()
+
     return OrderedDict([
         ('item', result),
         ('meta', pagging)
     ])
 
+
+def one_product(request, id):
+    extra_sql = f"""
+        select ann.id, fullname, phone_number, coalesce(price->>'from', '') || '-' ||coalesce(price->>'to', '') as price,
+         description, geo."name"->>'uz' as region_name_uz, geo."name"->>'ru' as region_name_ru, 
+         cat."name"->>'uz' as category_name_uz, cat."name"->>'ru' as category_name_ru
+    from tg_announce ann
+    left join geo_region geo on ann.region_id = geo.id
+    left join tg_announcecategories acat on ann.id = acat.resume_id 
+    left join tg_category cat on acat.category_id = cat.id 
+    where ann.id = %s and ann.is_active = True
+    """
+
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(extra_sql, [id])
+        data = dictfetchone(cursor)
+        if data:
+            result = _format(data)
+        else:
+            result = None
+
+    return OrderedDict([
+        ('item', result)
+    ])
 
 
 def _format_(data):
@@ -113,11 +140,11 @@ def _format_(data):
 
 def _format(data):
     return OrderedDict([
-        ('fullanme', data['fullname']),
-        ('price_from', data['price_from']),
-        ('price_to', data['price_to']),
+        ('id', data['id']),
+        ('fullname', data['fullname']),
         ('description', data['description']),
         ('price', data['price']),
+        ('phone_number', data['phone_number']),
         ('region_uz', data['region_name_uz']),
         ('region_ru', data['region_name_ru']),
         ('category_uz', data['category_name_uz']),
